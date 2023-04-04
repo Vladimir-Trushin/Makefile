7eedba049f037466bfb6c397da69df975cde1f3b
Vladimir Trushin <vtws21@vt-asus.local.net>
2023-04-04 22:20:35 +0300

    add c_new

---
+++ c_new/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+#include "text_line.h"
+#include "editor.h"
+
+
+int main()
+{
+    text_line_print();
+    editor_print();
+
+    return 0;
+}
---
+++ c_new/text_line/text_line.h
@@ -0,0 +1,6 @@
+#ifndef TEXT_LINE_H
+#define TEXT_LINE_H
+
+void text_line_print();
+
+#endif
