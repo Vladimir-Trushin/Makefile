3f6794cfa5ade1c0f6a1c49b23d7ff50eeb59df6
Vladimir Trushin <vtws21@vt-asus.local.net>
2023-04-05 15:16:37 +0300

    add c_cpp_temp

---
+++ c_cpp_temp/editor/editor.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "editor.h"
+
+void editor_print()
+{
+    printf("%s", "editor\n");
+}
---
+++ c_cpp_temp/text_line/text_line.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "text_line.h"
+
+void text_line_print()
+{
+    printf("%s", "text_line\n");
+}
